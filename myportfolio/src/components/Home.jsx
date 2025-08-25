f1ad093080402816f9c04465d413e1c9f8c61981
sumitjamdhade <sumitvjamdhade@gmail.com>
2025-08-25 19:36:02 +0530

    added navigation bar

---
+++ myportfolio/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen bg-background text-amber-50 overflow-x-hidden '>
+        <Navbar />
+    </div>
+  )
+}
+
+export default Home
---
+++ myportfolio/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="nav-conatainer h-15 bg-[#0f0f0f] text-[18px] w-screen flex justify-between items-center px-9 rounded-full m-2">
+        <div className="logo font-bold text-yellow-200">sumitjamdhade</div>
+        <div className="navigate text-amber-100">
+
+          <p>navigate</p>
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
